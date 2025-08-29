0bd5a827539843631934953f2786538b0e06dddb
subornakter <subornakter01@gmail.com>
2025-08-29 21:22:12 +0600

    some changes

--- hotline.js
+++ hotline.js
@@ -27,7 +27,7 @@ function getElement(id) {
     //  console.log(serviceNumber);
     const serviceNumber=callButton.parentNode.parentNode.children[1].children[2].innerText;
     // console.log(serviceNumber);
-      alert(`Calling 📞 ${serviceName} (${serviceNumber})...`);
+      alert(`📞Calling ${serviceName} (${serviceNumber})...`);
       const historyContainer=getElement("History-container");
       const historyDiv=document.createElement("div");
       historyDiv.innerHTML=`
